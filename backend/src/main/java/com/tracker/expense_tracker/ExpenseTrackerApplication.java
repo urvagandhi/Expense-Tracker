38d2ee21643848380aa3ca394e545282782c62b2
urvagandhi <urvagandhi24@gmail.com>
2025-07-03 10:30:00 +0530

    Initial project setup with Spring Boot 3.4.1

---
+++ backend/src/main/java/com/tracker/expense_tracker/ExpenseTrackerApplication.java
@@ -0,0 +1,16 @@
+package com.tracker.expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerApplication.class, args);
+	}
+	
+	
+
+}
