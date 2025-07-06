e2853cd76d057775417bc2de5d4dc264eff1f031
urvagandhi <urvagandhi24@gmail.com>
2025-07-06 14:15:00 +0530

    Add User entity with JPA mappings

---
+++ backend/src/main/java/com/tracker/expense_tracker/entity/User.java
@@ -0,0 +1,92 @@
+package com.tracker.expense_tracker.entity;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	@Column(nullable = false)
+	private String username;
+	
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+		
+	
+	@Column(nullable = false)
+	private String role = "USER";
+	
+	@OneToMany(mappedBy = "user" , cascade = CascadeType.ALL)
+	private List<Expense> expense;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public List<Expense> getExpense() {
+		return expense;
+	}
+
+	public void setExpense(List<Expense> expense) {
+		this.expense = expense;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
